a83a78bfc9c3dce13140f965ded32ad5732e0152
AI Apostle <ikhegbe9@gmail.com>
2026-01-17 15:37:26 +0100

    changes

--- scripts/app.py
+++ scripts/app.py
@@ -10,8 +10,8 @@ import streamlit as st
 
 # this is the interface of the app
 st.title("BOTANIQ")
-st.sidebar.title("About") 
-st.write("Upload an image of a plant leaf to detect diseases.")
+#st.sidebar.title("About") 
+st.write("Upload an image of a Tomato plant leaf to detect disease.")
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"]) 
 process_button = st.button("submit",help="use this button to send ")
 
